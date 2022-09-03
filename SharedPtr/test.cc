65b13ef656d0d60e657941e646e20e8602fcc4dc
SwordJoe <2410330450@qq.com>
2022-09-03 17:43:17 +0800

    add SharedPtr

---
+++ SharedPtr/test.cc
@@ -0,0 +1,17 @@
+#include"SharedPtr.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    SharedPtr<int> sp1(new int(100));
+    SharedPtr<int> sp2(sp1);
+    SharedPtr<int> sp3(sp1);
+    SharedPtr<int> sp4(sp1);
+    cout<<"sp1.useCnt()="<<sp1.useCnt()<<endl;
+    SharedPtr<int> sp_0(std::move(sp1));
+    cout<<"sp_0.useCnt()="<<sp_0.useCnt()<<endl;
+    SharedPtr<int> sp_1(std::move(sp2));
+    cout<<"sp_1.useCnt()="<<sp_1.useCnt()<<endl;
+}
